4dff76ea6c2add2df3f0c59c9449c5014db46a87
Filipe Silva do Nascimento <123flipe.nasc3@gmail.com>
2024-07-16 17:52:14 -0300

    Adicionei ContasControllers e atualizei o UsuariosControllers

---
+++ Api/Controllers/UsuariosControllers.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosControllers : ControllerBase
+    {
+        // POST: api/usuarios
+        [HttpPost]
+        public void Registrar([FromBody] Usuario usuario) {
+            //Implementação de cadastro do usuário
+        }
+        // GET: api/usuarios/{id}
+        [HttpGet("{id}")]
+        public void ObterPelaId(Guid id) {
+            //Implementação de consulta do usuário
+        }
+        // PUT: api/usuarios/{id}
+        [HttpPut("{id}")]
+        public void AtualizarPelaId(Guid id) {
+
+        }
+        [HttpDelete("{id}")]
+        public void DeletarPelaId(Guid id) {
+
+        }
+    }
+}
