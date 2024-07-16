6d80b158e415e88ce0faefaff0ce90bfb382dba3
Filipe Silva do Nascimento <123flipe.nasc3@gmail.com>
2024-07-16 17:15:59 -0300

    adicionei o swagger

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,13 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi.Models;
 
+var builder = WebApplication.CreateBuilder(args);
 //Adicione suporte .
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minha API", Version = "v1"});
+});
 
 var app = builder.Build();
-
-
 //Reconhça todos os arquivos que são controllers na minha aplicação.
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("v1/swagger.json", "Minha API V1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
